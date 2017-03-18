a81f0ca61035c29508cd37eed69f680f3817aed3
VashonHu <hu1251562003@gmail.com>
2017-03-18 20:43:11 +0800

    change charter

--- app/main/views.py
+++ app/main/views.py
@@ -96,7 +96,7 @@ def post(id):
         db.session.add(comment)
         db.session.commit()
         flash('Your comment has been published.')
-        return redirect(url_for('.post', id=post.id, page=-1))
+        return redirect(url_for('.post', id=post.id, page=-1))#id
     page = request.args.get('page', 1, type=int)
     if page == -1:
         page = (post.comments.count() - 1) \
---
+++ migrations/versions/294d014a89a4_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 294d014a89a4
+Revises: 
+Create Date: 2017-03-18 20:34:39.785946
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '294d014a89a4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
